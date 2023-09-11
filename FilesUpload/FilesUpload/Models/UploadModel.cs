1f62bda1706f3769a44e1e3e9bf8aad8d872744d
Vitalij Hnativ <vitalijhnativ@gmail.com>
2023-09-11 18:08:30 +0300

    Error messages adjustments

--- FilesUpload/FilesUpload/Models/UploadModel.cs
+++ FilesUpload/FilesUpload/Models/UploadModel.cs
@@ -5,11 +5,11 @@ namespace FilesUpload.Models
 {
     public class UploadModel
     {
-        [Required(ErrorMessage = "Please select a .docx file.")]
+        [Required(ErrorMessage = "File is required.")]
         [DataType(DataType.Upload)]
         public IBrowserFile File { get; set; } = null!;
 
-        [Required(ErrorMessage = "Please enter a valid email.")]
+        [Required(ErrorMessage = "Email is required.")]
         [EmailAddress(ErrorMessage = "Invalid email format.")]
         public string Email { get; set; } = string.Empty;
     }
