7d420aaa310ad1b709fe629ad421e95ddf366249
Vitalij Hnativ <vitalijhnativ@gmail.com>
2023-09-11 18:00:37 +0300

    Add UploadModel class

---
+++ FilesUpload/FilesUpload/Models/UploadModel.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Components.Forms;
+using System.ComponentModel.DataAnnotations;
+
+namespace FilesUpload.Models
+{
+    public class UploadModel
+    {
+        [Required(ErrorMessage = "Please select a .docx file.")]
+        [DataType(DataType.Upload)]
+        public IBrowserFile File { get; set; } = null!;
+
+        [Required(ErrorMessage = "Please enter a valid email.")]
+        [EmailAddress(ErrorMessage = "Invalid email format.")]
+        public string Email { get; set; } = string.Empty;
+    }
+}
